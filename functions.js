bfa3a266aebe713eea049e5bb6de6ab777a8b2e1
Aaron Bishop <aaronbishop.contact@gmail.com>
2020-08-20 14:04:34 -0400

    status add

--- functions.js
+++ functions.js
@@ -8,35 +8,3 @@ function imageFunction(display,changeInto){
 
 
 // do down here
-function preventFormSubmit() {
-    var forms = document.querySelectorAll('form');
-    for (var i = 0; i < forms.length; i++) {
-      forms[i].addEventListener('submit', function(event) {
-      event.preventDefault();
-      });
-    }
-  }
-  window.addEventListener('load', preventFormSubmit);    
-      
-      
-  function handleFormSubmit(formObject) {
-    google.script.run.processForm(formObject);
-    document.getElementById("myForm").reset();
-  }
-  function include(filename) {
-    return HtmlService.createHtmlOutputFromFile(filename)
-        .getContent();
-  }
-  /* @Process Form */
-  function processForm(formObject) {
-    var url = "https://docs.google.com/spreadsheets/d/1y8xY6-mcrjE_TWrvw6rcHPQZS8S9QXaqzRoUuP7_9kg/edit#gid=0";
-    var ss = SpreadsheetApp.openByUrl(url);
-    var ws = ss.getSheetByName("Data");
-    
-    ws.appendRow([formObject.first_name,
-                  formObject.last_name,
-                  formObject.gender,
-                  formObject.dateOfBirth,
-                  formObject.email,
-                  formObject.phone]);
-  }
