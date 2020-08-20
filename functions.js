b25eed79880ab6764c98040903a74c4e760cbe70
Aaron Bishop <aaronbishop.contact@gmail.com>
2020-08-20 11:26:59 -0400

    more update

--- functions.js
+++ functions.js
@@ -1,22 +1,42 @@
-let timesClicked = 0;
-function hideButtons(){
-    console.log("asdasdasdasdasd");
-    let butn = document.getElementById('hide-me');
-    if (butn.style.display === "none"){
-    butn.style.display = "flex";
-    }else{
-    butn.style.display = "none";
-    }
-}
 
-function easterEgg(){
-    timesClicked += 1;
-    console.log(timesClicked);
-    if (timesClicked >= 30){
-        document.getElementById('check-us-out').innerHTML = "Website by Aaron Bishop, Jivan Baghsarian, Olivia Chiavegato"
-    }
-}
 
 function imageFunction(display,changeInto){
     document.getElementById(display).src = changeInto;
 }
+
+
+
+
+// do down here
+function preventFormSubmit() {
+    var forms = document.querySelectorAll('form');
+    for (var i = 0; i < forms.length; i++) {
+      forms[i].addEventListener('submit', function(event) {
+      event.preventDefault();
+      });
+    }
+  }
+  window.addEventListener('load', preventFormSubmit);    
+      
+      
+  function handleFormSubmit(formObject) {
+    google.script.run.processForm(formObject);
+    document.getElementById("myForm").reset();
+  }
+  function include(filename) {
+    return HtmlService.createHtmlOutputFromFile(filename)
+        .getContent();
+  }
+  /* @Process Form */
+  function processForm(formObject) {
+    var url = "https://docs.google.com/spreadsheets/d/1y8xY6-mcrjE_TWrvw6rcHPQZS8S9QXaqzRoUuP7_9kg/edit#gid=0";
+    var ss = SpreadsheetApp.openByUrl(url);
+    var ws = ss.getSheetByName("Data");
+    
+    ws.appendRow([formObject.first_name,
+                  formObject.last_name,
+                  formObject.gender,
+                  formObject.dateOfBirth,
+                  formObject.email,
+                  formObject.phone]);
+  }
